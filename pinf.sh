87e43afe2a1f9b16880e3827fd7fdfca05a76caa
Christoph Dorn <christoph@christophdorn.com>
2015-06-21 00:12:50 -0700

    0.1.18

--- pinf.sh
+++ pinf.sh
@@ -7,19 +7,55 @@ function init {
 	local __BO_DIR__="$___TMP___"
 
 
-	BO_run_node "$__BO_DIR__/pinf.js" $@
+	function provisionAndInstallHarness {
 
+		BO_format "$VERBOSE" "HEADER" "Provisioning and installing pinf.sh harness"
 
-	BO_run_npm install --production
+		BO_run_node "$__BO_DIR__/pinf.js" $@
 
+		BO_format "$VERBOSE" "FOOTER"
+	}
 
-	if [ ! -e ".pgs/.provisioned" ]; then
-		BO_callPlugin "bash.origin.pinf@0.1.8" ensure genesis $@
-	fi
+	function ensurePGS {
 
-	export PIO_PROFILE_PATH="$__BO_DIR__/empty.profile.json"
+		BO_format "$VERBOSE" "HEADER" "Ensuring PINF.Genesis is provisioned"
 
-	./boot turn $@
+		export BO_PLUGIN_SEARCH_DIRPATHS="$__BO_DIR__/node_modules"
+
+		if [ ! -e ".pgs/.provisioned" ]; then
+			BO_callPlugin "bash.origin.pinf@0.1.8" ensure genesis $@
+		fi
+
+		BO_format "$VERBOSE" "FOOTER"
+	}
+
+	function turnOnce {
+
+		BO_format "$VERBOSE" "HEADER" "Turning system once"
+
+		export PIO_PROFILE_PATH="$__BO_DIR__/empty.profile.json"
+
+		./boot turn $@
+
+		BO_format "$VERBOSE" "FOOTER"
+	}
+
+	function welcome {
+
+		BO_format "$VERBOSE" "HEADER" "All Done! Welcome."
+		BO_log "1" ""
+		BO_log "1" "To get started run:"
+		BO_log "1" ""
+		BO_log "1" "  source bin/activate.sh"
+		BO_log "1" ""
+		BO_format "$VERBOSE" "FOOTER"
+
+	}
+
+	provisionAndInstallHarness
+	ensurePGS
+	turnOnce
+	welcome
 
 }
 init $@
